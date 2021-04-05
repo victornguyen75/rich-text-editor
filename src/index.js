ceabbe48dee042eebc7fc9207a38a39742961fe0
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-04-05 16:12:21 -0500

    feat(index.html, index.js): enable font editing via document.execCommand()

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const output = document.getElementById("output");
+const buttons = document.getElementsByClassName("tool--btn");
+
+for (let btn of buttons) {
+  btn.addEventListener("click", () => {
+    let cmd = btn.dataset["command"];
+    if (cmd === "createLink") {
+      let url = prompt("Enter the link here: ", "http://");
+      document.execCommand(cmd, false, url);
+      output.focus();
+    } else {
+      document.execCommand(cmd, false, null);
+      output.focus();
+    }
+  });
+}
